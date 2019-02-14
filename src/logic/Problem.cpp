fcb50b873cecbba7fdab4c2d273786ff5327c9b0
Eilers,R.R. (Renate) <r.r.eilers@students.uu.nl>
2019-02-14 16:28:10 +0100

    Names lemmas and adds options for z3

--- src/logic/Problem.cpp
+++ src/logic/Problem.cpp
@@ -8,7 +8,9 @@
 namespace logic {
     
     void Problem::outputSMTLIB(std::ostream& ostr)
-    {        
+    {       
+
+        ostr << "(set-option :produce-unsat-cores true)\n";
         // output sort declarations
         for(const auto& pair : Sorts::nameToSort())
         {
@@ -22,21 +24,27 @@ namespace logic {
         }
         
         // output each axiom
+        int ctr = 0;
         for (const auto& axiom : axioms)
         {
-            ostr << "\n(assert\n" << axiom->toSMTLIB(3) + "\n)\n";
+            ostr << "\n(assert\n (!\n" << axiom->toSMTLIB(3) + "\n :named aixiom" << ctr << "))\n";
         }
 
         // output each lemma
+        ctr = 0;
         for (const auto& lemma : lemmas)
         {
             // TODO: improve handling for lemmas:
             // custom smtlib-extension
-            ostr << "\n(assert\n" << lemma->toSMTLIB(3) + "\n)\n";
+            ostr << "\n(assert\n (!\n" << lemma->toSMTLIB(3) + "\n :named lemma" << ctr << "))\n";
+            ctr++;
         }
         
         // output conjecture
         assert(conjecture != nullptr);
         ostr << "\n(assert-not\n" << conjecture->toSMTLIB(3) + "\n)\n";
+
+        ostr << "(check-sat)\n";
+        ostr << "(get-unsat-core)\n";
     }
 }
